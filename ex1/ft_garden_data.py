7a8a0edad2d131af3ac8a94aab84a1a4b5fa9bed
lelero17 <leandro-emmerling@web.de>
2026-03-24 10:29:39 +0100

    fix: add show method to ft_garden_data to match updated subject

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -4,10 +4,20 @@
 class Plant:
     """Represent a plant with basic attributes."""
     def __init__(self, name, height, age):
-        """Initialize a plant with name, height, and age."""
+        """Initialize a plant with his attributes.
+
+        Args:
+            name: Name of the Plant.
+            height: height of the Plant
+            age: age of the Plant
+        """
         self.name = name
         self.height = height
         self.age = age
+
+    def show(self):
+        """Displays the plant information."""
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
 if __name__ == "__main__":
@@ -18,4 +28,4 @@ if __name__ == "__main__":
     ]
     print("=== Garden Plant Registry ===")
     for plant in plants:
-        print(f"{plant.name}: {plant.height}cm, {plant.age} days old")
+        plant.show()
