6866fa1f74fcd1ecdc1396d9eb7ecd5e554deb6b
lelero17 <leandro-emmerling@web.de>
2026-03-29 15:07:39 +0200

    feat: add Tree class with produce shade functionality and Vegetable class with age and grow funcitonality

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -13,7 +13,7 @@ class Plant:
         """
         self._name = name
         self._height = 0
-        self._age = 0
+        self._age_in_days = 0
         self.set_height(height)
         self.set_age(age)
 
@@ -23,7 +23,7 @@ class Plant:
 
     def get_age(self):
         """Return the plant's age."""
-        return (self._age)
+        return (self._age_in_days)
 
     def set_height(self, new_height):
         """Set the plant's height with validation.
@@ -48,21 +48,21 @@ class Plant:
             return (f"{self._name}: Error, age can't be negative\n"
                     f"Age update rejected")
         else:
-            self._age = new_age
-            return (f"Age updated: {self._age} days")
+            self._age_in_days = new_age
+            return (f"Age updated: {self._age_in_days} days")
 
     def grow(self):
         """Increase plant height by 0.8cm."""
         self._height += 0.8
         self._height = round(self._height, 1)
 
-    def age_one_day(self):
+    def age(self):
         """Increase plant age by 1 day."""
-        self._age += 1
+        self._age_in_days += 1
 
     def show(self):
         """Return plant information in factory format."""
-        return (f"{self._name}: {self._height}cm, {self._age} days old")
+        print(f"{self._name}: {self._height}cm, {self._age_in_days} days old")
 
 
 class Flower(Plant):
@@ -81,32 +81,89 @@ class Flower(Plant):
         self.has_bloomed = False
 
     def show(self):
-        """Return plant information in factory format."""
-        plant_show = super().show()
-        if self.has_bloomed is False:
-            return (f"{plant_show} \n Color: {self.color}\n"
-                    f" {self._name} has not bloomed yet\n"
-                    f"[asking the rose to bloom]")
+        """Return flower information in factory format."""
+        super().show()
+        print(f" Color: {self.color}")
+        if self.has_bloomed:
+            print(f" {self._name} is blooming beautifully!")
         else:
-            return (f"{plant_show} \n Color: {self.color}"
-                    f"\n {self._name} is blooming beautifully")
+            print(f" {self._name} has not bloomed yet")
 
     def bloom(self):
         """Make the flower bloom."""
         self.has_bloomed = True
+        
 
 
 class Tree(Plant):
     """Represent a tree with trunk diameter and shade production."""
+    def __init__(self, name, height, age, trunk_diameter):
+        """Initialize a Tree with his attributes
+        
+        Args:
+            name: Name of the Tree.
+            height: height of the Tree.
+            age: age of the Tree
+            trunk_diameter: the width of the tree's stem
+        """
+        super().__init__(name, height, age)
+        self.trunk_dia = trunk_diameter
+        self.shade = False
+        
+    def show(self):
+        """Return tree information in factory format."""
+        super().show()
+        print(f" Trunk diameter: {self.trunk_dia}cm") 
+            
+    def produce_shade(self):
+        """Make the tree produce a shade."""
+        print(f"Tree {self._name} now produces a shade of "
+              f"{self._height}cm long and {self.trunk_dia}cm wide.")
 
 
 class Vegetable(Plant):
     """Represent a vegetable with harvest season and nutritional value."""
+    def __init__(self, name, height, age, harvest_season, nutritional_value):
+        """Initialize a Vegetable with his attributes
+        
+        Args:
+            name: Name of the Vegetable.
+            height: height of the Vegetable.
+            age: age of the Vegetable.
+            harvest_season: the time when to collect the vegetable.
+            nutritional_value: Initial nutritional value of the vegetable.
+        """
+        super().__init__(name, height, age)
+        self.harvest_season = harvest_season
+        self.nutritional_value = nutritional_value
 
+    def show(self):
+        """Return plant information in factory format."""
+        super().show()
+        print(f" Harvest season: {self.harvest_season}")
+        print(f" Nutritional value: {self.nutritional_value}")
+    
+    def age(self):
+        """Increase plant age by 1 day."""
+        super().age()
+        self.nutritional_value += 1
+        
+        
 
 if __name__ == "__main__":
-    print("=== Garden Plant Types === \n== Flower")
+    print("=== Garden Plant Types === \n=== Flower")
     flower = Flower("Rose", 15.0, 10, "red")
-    print(flower.show())
+    tree = Tree("Oak", 200.0, 365, 5.0)
+    vegetable = Vegetable("Tomato", 5.0, 10, "April", 0)
+    flower.show()    
     flower.bloom()
-    print(flower.show())
+    flower.show()
+    print("\n=== Tree")
+    tree.show()
+    tree.produce_shade()
+    print("\n=== Vegetable")
+    vegetable.show()
+    for i in range(1, 21):
+        vegetable.grow()
+        vegetable.age()
+    vegetable.show()
